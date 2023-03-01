2fb88221527aa4a53256098389b02071b30fcfd8
Christian Sanabria <csanabria@itcr.ac.cr>
2023-02-28 20:39:22 -0600

    Final Día 1

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return VillaStore.VillaList;
+        }
+
+        [HttpGet("id")]
+        public VillaDto GetVilla(int id)
+        {
+            return VillaStore.VillaList.FirstOrDefault(v => v.Id == id);
+        }
+    }
+}
---
+++ MagicVilla_API/Datos/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_API.Modelos.Dto;
+
+namespace MagicVilla_API.Datos
+{
+    public class VillaStore
+    {
+        public static List<VillaDto> VillaList = new List<VillaDto>
+        {
+            new VillaDto{Id=1,Nombre="Vista a la Piscina"},
+            new VillaDto{Id=2,Nombre="Vista a la Playa"}
+        };
+    }
+}
